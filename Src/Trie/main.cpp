16f30bd485ef36e4b1bafaae0ceef2044c1e78aa
Christian Popescu <christian.popescu@outlook.com>
2019-11-03 23:51:32 +0100

    First commit on Tries

---
+++ Src/Trie/main.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+#include <iostream>
+
+class TrieNode {
+    char value;
+    TrieNode* children[26];
+    bool isTerminal;
+};
+int main() {
+    std::cout << "Hello, World!" << std::endl;
+    return 0;
+}
