296f652b6475b01ba5a54573f60b73ca1c00d545
Christian Popescu <christian.popescu@outlook.com>
2019-11-10 15:28:33 +0100

    add iterator

--- Src/Trie/main.cpp
+++ Src/Trie/main.cpp
@@ -3,6 +3,10 @@
 
 #define INDEX(chr) chr - 'a'
 
+class TrieIterator {
+    TrieIterator
+};
+
 class TrieNode {
 public:
     char value;
@@ -13,6 +17,7 @@ public:
         isTerminal = false;
         for (int i=0; i<26; ++i) children[i] = nullptr;
     }
+    friend TrieIterator;
 };
 
 class TrieAsWordList {
@@ -39,6 +44,26 @@ public:
         current->theWord = wordToAdd;
     }
 };
+
+class TrieIterator {
+
+public:
+    TrieIterator (TrieNode* root) {
+        first = current = root;
+    }
+    void First() {
+        current = first;
+    }
+
+    TrieNode* Current() {
+        return current;
+    }
+
+protected:
+    TrieNode* first;
+    TrieNode* current;
+};
+
 int main() {
     std::cout << "Hello, World!" << std::endl;
     TrieAsWordList trieAsWordList;
