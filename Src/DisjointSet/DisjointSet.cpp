157c601a8d56ec018d1ffcc655f946ebdd1f0d52
Christian Popescu <christian.popescu@outlook.com>
2019-11-23 15:57:17 +0100

    add disjoint set

---
+++ Src/DisjointSet/DisjointSet.cpp
@@ -0,0 +1,49 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+vector<int> merge2Ways(const vector<int>& lst1, const vector<int> lst2){
+  int n1(lst1.size());
+  int n2(lst2.size());
+
+  vector<int> result;
+
+  int i1(0);
+  int i2(0);
+
+  while (i1 < n1 && i2 < n2) {
+      if (lst1[i1] == lst2[i2] ) {
+	result.push_back(lst1[i1]);
+			 ++i1;
+			 ++i2;
+      } else if (lst1[i1] < lst2[i2] ) {
+	result.push_back(lst1[i1]);
+			 ++i1;
+      } else {
+	result.push_back(lst2[i2]);
+			 ++i2;
+	}
+	
+    }
+    if (i1 < n1) {
+      while (i1 < n1) {
+	result.push_back(lst1[i1]);
+		++i1;
+		}
+	} else {
+      while (i2 < n2) {
+	result.push_back(lst2[i2]);
+			 ++i2;
+      }
+    }
+  return move(result);
+  
+}
+int main(){ 
+  vector<int> v1 = {2, 4, 7, 9};
+  vector<int> v2 = {2, 3, 4, 9, 13, 15};
+
+  for(auto i : merge2Ways(v1, v2)) cout << i << endl;
+  cout << "End of program" << endl;
+}
