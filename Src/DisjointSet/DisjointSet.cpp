d52526187c7801e890dd933626262715f873da76
Christian Popescu <christian.popescu@outlook.com>
2019-11-23 17:06:06 +0100

    Disjoint set with shared pointers

--- Src/DisjointSet/DisjointSet.cpp
+++ Src/DisjointSet/DisjointSet.cpp
@@ -1,8 +1,7 @@
 #include <iostream>
 #include <vector>
-
+#include <memory>
 using namespace std;
-
 vector<int> merge2Ways(const vector<int>& lst1, const vector<int> lst2){
   int n1(lst1.size());
   int n2(lst2.size());
@@ -40,8 +39,42 @@ vector<int> merge2Ways(const vector<int>& lst1, const vector<int> lst2){
   return move(result);
   
 }
+
+class SetNode {
+public:
+	shared_ptr<vector<int>> pSet;
+	int nbElem;	
+};
+
+vector<SetNode> DisjointSet(10);
+
+void MakeDisjointSet(vector<SetNode>& ds) {
+	for (int i=0; i < ds.size(); ++i){
+		ds[i].pSet = make_shared<vector<int>>(i,1);
+		ds[i].nbElem = 1;
+	} 
+}
+
+void Union(int set1, int set2) {
+	if (DisjointSet[set1].pSet == DisjointSet[set2].pSet ) return; // nothing to do
+	if (DisjointSet[set1].nbElem > DisjointSet[set2].nbElem ){
+	  DisjointSet[set1].pSet = &merge2Ways(*(DisjointSet[set1].pSet), *(DisjointSet[set2].pSet));
+		DisjointSet[set1].nbElem =DisjointSet[set1].pSet->size();
+		 DisjointSet[set2].pSet =  DisjointSet[set1].pSet;
+		DisjointSet[set2].nbElem =DisjointSet[set2].pSet->size();
+	} else {
+
+    	DisjointSet[set2].pSet = &merge2Ways(DisjointSet[set1].pSet, DisjointSet[set2].pSet);
+		DisjointSet[set2].nbElem =DisjointSet[set2].pSet->size();
+		DisjointSet[set1].pSet =  DisjointSet[set2].pSet;
+		DisjointSet[set1].nbElem =DisjointSet[set1].pSet->size();
+	}
+}
+
 int main(){ 
-  vector<int> v1 = {2, 4, 7, 9};
+  
+		
+	vector<int> v1 = {2, 4, 7, 9};
   vector<int> v2 = {2, 3, 4, 9, 13, 15};
 
   for(auto i : merge2Ways(v1, v2)) cout << i << endl;
