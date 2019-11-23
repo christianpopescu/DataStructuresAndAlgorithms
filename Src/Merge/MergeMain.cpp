6b4dc770fb259528b0adea714113157d89522191
Christian Popescu <christian.popescu@outlook.com>
2019-11-23 14:36:01 +0100

    merge

--- Src/Merge/MergeMain.cpp
+++ Src/Merge/MergeMain.cpp
@@ -1,15 +1,48 @@
 #include <iostream>
-#include "SimpleLinkedList"
-int main(){
-  using namespace std;
-  SingleLinkedList<int> sll;
-  cout << sll.empty() << endl;
-   sll.AddFront(10);
-    sll.AddFront(20);
-  //  sll.AddFront(30);
-  //  while ( !sll.empty()){
-    sll.Front() ;
-    //   sll.RemoveFront();
-    // }
+#include <vector>
+
+using namespace std;
+
+vector<int> merge2Ways(const vector<int>& lst1, const vector<int> lst2){
+  int n1(lst1.size());
+  int n2(lst2.size());
+
+  vector<int> result;
+
+  int i1(0);
+  int i2(0);
+
+  while (i1 < n1 && i2 < n2) {
+      if (lst1[i1] == lst2[i2] ) {
+	result.push_back(lst1[i1]);
+			 ++i1;
+			 ++i2;
+      } else if (lst1[i1] < lst2[i2] ) {
+	result.push_back(lst1[i1]);
+			 ++i1;
+      } else {
+	result.push_back(lst2[i2]);
+			 ++i2;
+	}
+	
+    }
+    if (i1 < n1) {
+      while (i1 < n1) {
+	result.push_back(lst1[i1]);
+		++i1;
+		}
+	} else {
+      while (i2 < n2) {
+	result.push_back(lst2[i2]);
+			 ++i2;
+      }
+    }
+  return move(result);
+  
+}
+int main(){ 
+  vector<int> v1 = {2, 4, 7};
+  vector<int> v2 = {2, 3, 4};
+  
   cout << "End of program" << endl;
 }
