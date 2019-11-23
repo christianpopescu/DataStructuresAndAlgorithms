ffec9efd9445938f41d82a322f02d0f7ad8fc6f2
Christian Popescu <christian.popescu@outlook.com>
2019-11-23 23:36:00 +0100

    union join implemented as sequence

--- Src/DisjointSet/DisjointSet.cpp
+++ Src/DisjointSet/DisjointSet.cpp
@@ -2,84 +2,40 @@
 #include <vector>
 #include <memory>
 using namespace std;
-vector<int>*  merge2Ways(vector<int>* lst1, vector<int>* lst2){
-  int n1((*lst1).size());
-  int n2((*lst2).size());
 
-  vector<int>* result = new vector<int>();
 
-  int i1(0);
-  int i2(0);
+vector<vector<int>*> DisjointSet(10);
 
-  while (i1 < n1 && i2 < n2) {
-    if ((*lst1)[i1] == (*lst2)[i2] ) {
-      result->push_back((*lst1)[i1]);
-			 ++i1;
-			 ++i2;
-    } else if ((*lst1)[i1] < (*lst2)[i2] ) {
-      result->push_back((*lst1)[i1]);
-			 ++i1;
-      } else {
-      result->push_back((*lst2)[i2]);
-			 ++i2;
-	}
-	
-    }
-    if (i1 < n1) {
-      while (i1 < n1) {
-	result->push_back((*lst1)[i1]);
-		++i1;
-		}
-	} else {
-      while (i2 < n2) {
-	result->push_back((*lst2)[i2]);
-			 ++i2;
-      }
-    }
-    delete(lst1);
-    delete(lst2);
-  return result;
-  
-}
-
-class SetNode {
-public:
-	vector<int>* pSet;
-	int nbElem;	
-};
-
-vector<SetNode> DisjointSet(10);
-
-void MakeDisjointSet(vector<SetNode>& ds) {
+void MakeDisjointSet(vector<vector<int>*>& ds) {
 	for (int i=0; i < ds.size(); ++i){
-		ds[i].pSet = new vector<int>(1,i);
-		ds[i].nbElem = 1;
+		ds[i] = new vector<int>(1,i);
 	} 
 }
 
 void Union(int set1, int set2) {
-	if (DisjointSet[set1].pSet == DisjointSet[set2].pSet ) return; // nothing to do
-	cout << DisjointSet[set1].nbElem << "  <   " << DisjointSet[set2].nbElem << endl;
-	if (DisjointSet[set1].nbElem > DisjointSet[set2].nbElem ){
-	  DisjointSet[set1].pSet = merge2Ways(DisjointSet[set1].pSet, DisjointSet[set2].pSet);
-	  cout << set1 <<  "Nb elem = " << (DisjointSet[set1].nbElem =DisjointSet[set1].pSet->size()) << endl;
-		 DisjointSet[set2].pSet =  DisjointSet[set1].pSet;
-	 cout << set2 <<  "Nb elem = " << (DisjointSet[set2].nbElem =DisjointSet[set2].pSet->size()) << endl;
-	} else {
+  if (set1 == set2) return;
+  int s,d;
+  vector<int>* tmp;
+  if (DisjointSet[set1]->size() > DisjointSet[set2]->size()) {
+    d = set1;
+    s = set2;
+    tmp = DisjointSet[set2];
+  } else {
+    d = set2;
+    s = set1;
+    tmp = DisjointSet[set1];
+  }
+  for (auto e : (*tmp)) {
+    DisjointSet[e] = DisjointSet[d];
+    DisjointSet[e]->push_back(e);
+  }
 
-    	DisjointSet[set2].pSet = merge2Ways(DisjointSet[set1].pSet, DisjointSet[set2].pSet);
-	cout <<  set2 << "Nb elem = " <<	(DisjointSet[set2].nbElem =DisjointSet[set2].pSet->size()) << endl;
-		DisjointSet[set1].pSet =  DisjointSet[set2].pSet;
-		DisjointSet[set1].nbElem =DisjointSet[set1].pSet->size();
-	}
-}
+  delete tmp;
 
-int Find(int i) {
-  return (*(DisjointSet[i].pSet))[0]; // return first element of set
 }
 
 int SizeSet(int i) {
-  return DisjointSet[i].nbElem; // return first element of set
+  return DisjointSet[i]->size(); 
 }
 
 
@@ -88,16 +44,23 @@ int main(){
   
   MakeDisjointSet(DisjointSet);
   for (int i=1; i<10; ++i) {
-    cout << SizeSet(i) << "set id =" << Find(i) <<  endl;
+    cout << SizeSet(i)  <<  endl;
   }
+  cout << "-------------------" << endl;
   Union(1,2);
   Union(1,3);
   cout << SizeSet(1) << endl;
   cout << SizeSet(2) << endl;
   cout << SizeSet(3) << endl;
-  cout << "------------------------" << endl;
-  cout << Find(2) << endl;
-  cout << Find(3) << endl;
-  cout << Find(4) << endl;
+  Union(5,7);
+  Union(7,8);
+  
+  cout << "-------------------" << endl;
+
+  for (int i=1; i<10; ++i) {
+    cout << SizeSet(i)  <<  endl;
+  }
+
+  
   cout << "End of program" << endl;
 }
