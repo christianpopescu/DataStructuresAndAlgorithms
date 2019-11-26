17c439d344c74a9b23fea21ae436a44c3d19aa17
Christian Popescu <christian.popescu@outlook.com>
2019-11-26 22:31:45 +0100

    Add trees sources

---
+++ Src/Trees/BinaryTrees/binary_tree.h
@@ -0,0 +1,8 @@
+template<typename T>
+class TreeNode {
+	public:
+	
+	TreeNode* left;
+	TreeNode* right;
+	T value;
+}
---
+++ Src/Trees/BinaryTrees/main_binary_tree.cc
@@ -0,0 +1,7 @@
+#include <iostream>
+#include "binary_tree.h"
+int main() {
+	
+	cout << "End of programm" << endl;
+	return 0;
+}
