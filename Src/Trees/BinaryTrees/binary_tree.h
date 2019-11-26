6b898ffbcf6582c4b395185def01bb62371479ad
Christian Popescu <christian.popescu@outlook.com>
2019-11-26 23:21:51 +0100

    binary tree simple

--- Src/Trees/BinaryTrees/binary_tree.h
+++ Src/Trees/BinaryTrees/binary_tree.h
@@ -1,8 +1,38 @@
+#include <iostream>
 template<typename T>
 class TreeNode {
 	public:
-	
+	TreeNode() = default;
+	explicit TreeNode(T value) : value(value) {
+		std::cout << "I am here" << std::endl;
+	}
 	TreeNode* left;
 	TreeNode* right;
 	T value;
+};
+
+template<typename T>
+class BinaryTree {
+	public:
+	BinaryTree() :root(nullptr) {}
+	void InsertValue(T value) {
+			std::cout << "I am here" << std::endl;
+		InsertValue(root, value);
+	}
+	private:
+	TreeNode<T>* root;
+	void InsertValue(TreeNode<T>* node, T value) {
+		std::cout << "insert value " << node <<std::endl;
+		if (node == nullptr) {
+			std::cout << "nullptr" << std::endl;
+			node = new TreeNode<T>(value);
+			std::cout << "nullptr end" << std::endl;
+		} else if (node->value > value) {
+			std::cout << "left" << std::endl;
+			InsertValue(node->left, value);
+		} else {
+			std::cout << "right" << std::endl;
+			InsertValue (node->right, value);
+		}
+	}
 };
--- Src/Trees/BinaryTrees/main_binary_tree.cc
+++ Src/Trees/BinaryTrees/main_binary_tree.cc
@@ -2,7 +2,9 @@
 #include "binary_tree.h"
 using namespace std;
 int main() {
-	
+	BinaryTree<int> bts;
+	cout << "bts insert value" << endl;
+	bts.InsertValue(10);
 	cout << "End of programm" << endl;
 	return 0;
 }
