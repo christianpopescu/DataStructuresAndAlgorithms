fe156bc247a0ac3d3f505e61172ef29067d0b908
Christian Popescu <christian.popescu@outlook.com>
2019-12-01 17:08:12 +0100

    Bellman Ford done

--- Src/Graphs/BellmanFordShortestPath/BellmanFord.cc
+++ Src/Graphs/BellmanFordShortestPath/BellmanFord.cc
@@ -16,11 +16,28 @@ std::vector<std::tuple<int,int,int>> edges;
 
 int main() {
 	using namespace std;
-	
+	int n{0};   // nodes number
+	int s{0};   // starting node
 	InputReader ir;
 	ir.SetInputStreamFile("input.txt");
+	cout << "Number of Nodes = " ;
+	cin >> n;
+	cout << endl;
+	vector<int> distance(n+1,INF);
+    cout << "Starting node = " ;
+	cin >> s;
+	cout << endl;
 	ir.ReadGraphAsEdges(edges);
-	
+	distance[s] = 0;
+	int a{0}, b{0}, w{0};
+	for (int i=1; i < n; ++i) {
+		for (auto e: edges) {
+			tie(a, b, w) = e;
+			distance[b] = min(distance[b], distance[a]!= INF ? distance[a] + w : INF);
+		}
+	}
+		
 	cout << edges.size();
+	for (int i=1; i<=n; ++i) cout << i << " min distance = " << distance[i] << endl;
 	
 }
